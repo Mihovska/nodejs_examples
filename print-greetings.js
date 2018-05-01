de0ade1ce871c8526b70e0a669b363c6a676694e
Mihovska <vili_mihovska@live.com>
2018-05-01 20:44:27 +0300

    initial commit

---
+++ print-greetings.js
@@ -0,0 +1,15 @@
+//greetings methods
+
+var sayHelloInEnglish = function () {
+    return 'Hello'
+}
+
+var sayHelloInSwedish = function () {
+    return 'Hej'
+}
+
+var sayHelloInTatar = function () {
+    return 'Isänme'
+}
+
+console.log('Swedish ' + sayHelloInSwedish() + ' & Englisg ' + sayHelloInEnglish() + ' & Tatar ' + sayHelloInTatar());
